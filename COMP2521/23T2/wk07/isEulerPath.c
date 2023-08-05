938f41dc7330fa4453f5d2ecf68ede146426d088
vvwangg <vivianwang292@gmail.com>
2023-08-05 23:04:57 +1000

    23T2 done

--- COMP2521/23T2/wk07/isEulerPath.c
+++ COMP2521/23T2/wk07/isEulerPath.c
@@ -8,13 +8,46 @@ struct edge {
 
 struct graph {
     int nE;
-    struct edge** edges;
+    int** edges;
 };
 
 // Checks if a supplied array of Edges is an Euler path
 // e[] is an array of nE edges
 bool isEulerPath(Graph g, Edge e[], int nE) {
     assert(g != NULL);
+
+    // include all edges
+    if (g->nE != nE) {
+        return false;
+    }
+
+    // are the edges actually in the graph
+    for (int i = 0; i < nE; i++) {
+        if (g->edges[e[i]->v][e[i]->w] == 0) {
+            return false;
+        }
+    }
+
+    // is e[] actually a path 
+    for (int i = 0; i < nE - 1; i++) {
+        if (e[i]->w != e[i+1]->v) {
+            return false;
+        }
+    }
+
+    // include edges exactly once
+    for (int i = 0; i < nE; i++) {
+        for (int j = i + 1; j < nE; j++) {
+            Edge a = e[i];
+            Edge b = e[j];
+            if (a->v == b->v && a->w == b->w) {
+                // they are the same edge
+                return false; 
+            } else if (a->v == b->w && a->w == b->v) {
+                return false;
+            }
+        }
+    }
 
     return false;
 }
